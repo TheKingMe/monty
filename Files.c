aa4b26c55f42a8bd98f31cd2770ce1850b71f5a0
Ismail <im179242@gmail.com>
2023-10-18 20:29:14 +0100

    Read file

--- Files.c
+++ Files.c
@@ -126,3 +126,22 @@ int parse_line(char *buffer, int line_number, int format)
 	find_func(opcode, value, line_number, format);
 	return (format);
 }
+
+/**
+ * read_file - reads a file
+ * @fd: pointer to file descriptor
+ * Return: void
+ */
+
+void read_file(FILE *fd)
+{
+	int line_number, format = 0;
+	char *buffer = NULL;
+	size_t len = 0;
+
+	for (line_number = 1; getline(&buffer, &len, fd) != -1; line_number++)
+	{
+		format = parse_line(buffer, line_number, format);
+	}
+	free(buffer);
+}
