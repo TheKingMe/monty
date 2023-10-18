e695b9cdb09fdf33801ceeb62a55ce004ada6eb7
Ismail <im179242@gmail.com>
2023-10-18 19:36:01 +0100

    Find a function

--- Files.c
+++ Files.c
@@ -16,3 +16,29 @@ void open_file(char *file_name)
 	read_file(fd);
 	fclose(fd);
 }
+/**
+ * find_func - find the appropriate function for the opcode
+ * @opcode: opcode
+ * @value: argument of opcode
+ * @format:  storage format. If 0 Nodes will be entered as a stack.
+ * @ln: line number
+ * if 1 nodes will be entered as a queue.
+ * Return: void
+ */
+void find_func(char *opcode, char *value, int ln, int format)
+{
+	int i;
+	int flag;
+
+	instruction_t func_list[] = {
+		{"push", add_to_stack},
+		{"pall", print_stack},
+		{"pint", print_top},
+		{"pop", pop_top},
+		{"nop", nop},
+		{"swap", swap_nodes},
+		{"add", add_nodes},
+		
+		{NULL, NULL}
+	};
+}
